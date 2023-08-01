b41058294816905e594085cef4a4d20cdeda0aef
Dalvis <Dalvis.liz27@gmail.com>
2023-08-01 16:17:02 -0400

    created the getAll function for pokemon router

--- api/Trainers/trainers-router.js
+++ api/Trainers/trainers-router.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const Trainers = require('./trainers-model.js');
+const { validateIdByType } = require('../middleware');
 
 const router = express.Router();
+
+// GET all trainers
+router.get('/', (req, res) => {
+    Train
+});
+
+// GET trainer by id
+router.get('/:id', validateIdByType('trainers'), (req, res) => {
+
+});
+
+// GET all pokemon for a trainer
+router.get('/:id/pokemon', validateIdByType('trainers'), (req, res) => {
+
+});
+
+// POST new trainer
+router.post('/', (req, res) => {
+
+});
 
 module.exports = router;
